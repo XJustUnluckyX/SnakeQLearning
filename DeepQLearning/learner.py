f94a5eb9ba19ca5133a456107c976aaf28309a6d
Davide La Gamba <d.lagamba@studenti.unisa.it>
2024-01-30 18:24:49 +0100

    Add Grid Search for DeepQLearning, modify learner and env

--- DeepQLearning/learner.py
+++ DeepQLearning/learner.py
@@ -6,21 +6,20 @@ from deep_model import Linear_QNet, QTrainer
 from env import Direction, Point
 
 MAX_MEMORY = 100_000
-BATCH_SIZE = 1000
-LR = 0.001  # learning rate
-
-
-# TODO implementare una GridSearch per gli iper-parametri
 
 class QLearner:
 
-    def __init__(self):
-        self.epsilon = 0
-        self.gamma = 0.9
+    def __init__(self, epsilon, gamma, learning_rate, batch_size, epsilon_decay, epsilon_min):
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.gamma = gamma
         self.n_games = 0
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
 
         self.deep_model = Linear_QNet(11, 256, 3)  # input, hidden, output dim
-        self.q_trainer = QTrainer(self.deep_model, lr=LR, gamma=self.gamma)
+        self.q_trainer = QTrainer(self.deep_model, lr=self.learning_rate, gamma=self.gamma)
         self.memory = deque(maxlen=MAX_MEMORY)
 
     def get_state(self, game):
@@ -68,8 +67,8 @@ class QLearner:
         self.memory.append((state, action, reward, next_state, done))  # Pop dell'item a sx se si raggiunge MAX_MEMORY
 
     def long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE)  # estraggo un campione di tuple
+        if len(self.memory) > self.batch_size:
+            mini_sample = random.sample(self.memory, self.batch_size)  # estraggo un campione di tuple
         else:
             mini_sample = self.memory
 
@@ -80,9 +79,9 @@ class QLearner:
         self.q_trainer.train(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        self.epsilon = 80 - self.n_games #TODO provare con epsilon diversi
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
         last_move = [0, 0, 0]
-        if random.randint(0, 200) < self.epsilon:
+        if random.random() < self.epsilon:
             move = random.randint(0, 2)
             last_move[move] = 1
         else:
--- DeepQLearning/main.py
+++ DeepQLearning/main.py
@@ -2,33 +2,65 @@ from learner import QLearner
 from env import Env
 from plotter import plot
 
-plot_scores = []
-plot_mean_scores = []
-total_score = 0
-record = 0
-agent = QLearner()
-env = Env()
 
-while True:
-    state_old = agent.get_state(env)
-    final_move = agent.get_action(state_old)
-    reward, done, score = env.step(final_move)
-    state_new = agent.get_state(env)
-    agent.short_memory(state_old, final_move, reward, state_new, done)
-    agent.remember(state_old, final_move, reward, state_new, done)
 
-    if done:
-        env.reset()
-        agent.n_games += 1
-        agent.long_memory()
-        if score > record:  # Salvo le migliori partite
-            record = score
-            agent.deep_model.save_model()
+# Grid Search
+learning_rate = [0.001, 0.01, 0.1]
+gamma = [0.9, 0.7, 0.5]
+epsilon_start = [0.8, 0.6, 0.4]
+epsilon_decay = [0.95, 0.995, 0.9]
+epsilon_min = [0.03, 0.05, 0.1, 0.05]
+batch_size = [1000, 2000]
+best_parameters = []
 
-        print('Game', agent.n_games, 'Score', score, 'Record:', record)
+max_reward_grid_search = -100000
+rel_length = 0
 
-        plot_scores.append(score)
-        total_score += score
-        mean_score = total_score / agent.n_games
-        plot_mean_scores.append(mean_score)
-        plot(plot_scores, plot_mean_scores)
+for a in range(len(learning_rate)):
+    for b in range(len(gamma)):
+        for c in range(len(epsilon_start)):
+            for d in range(len(epsilon_decay)):
+                for e in range(len(epsilon_min)):
+                    for f in range(len(batch_size)):
+                        env = Env()
+                        # Parametri di addestramento
+                        num_episodes = 500
+                        max_reward = -10000
+                        length = 0
+                        temp_max_length = 0
+                        total_score = 0
+                        total_reward = 0
+                        plot_scores = []
+                        plot_mean_scores = []
+                        record = -1
+                        agent = QLearner(epsilon_start[c], gamma[b], learning_rate[a], batch_size[f], epsilon_decay[d], epsilon_min[e])
+                        # Ciclo di addestramento
+                        for episode in range(num_episodes):
+                            env.reset()
+                            while True:
+                                state_old = agent.get_state(env)
+                                last_move = agent.get_action(state_old)
+                                reward, done, score = env.step(last_move)
+                                state_new = agent.get_state(env)
+                                agent.short_memory(state_old, last_move, reward, state_new, done)
+                                agent.remember(state_old, last_move, reward, state_new, done)
+
+                                if done:
+                                    env.reset()
+                                    agent.n_games += 1
+                                    agent.long_memory()
+                                    if score > record:  # Salvo le migliori partite
+                                        record = score
+                                        agent.deep_model.save_model()
+
+                                    print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+                                    total_score += score
+                                    mean_score = total_score / agent.n_games
+                                    plot_scores.append(score)
+                                    plot_mean_scores.append(mean_score)
+                                    plot(plot_scores, plot_mean_scores)
+                                    break
+
+print(f"Max Reward Grid Search: {max_reward_grid_search}, Relative Length: {rel_length}")
+print(f"Best parameters: {best_parameters}")
