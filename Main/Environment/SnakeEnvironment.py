ccbc8b36e7ff4533910bbd69f3f97a8cb6227b8b
Davide La Gamba <d.lagamba@studenti.unisa.it>
2024-02-03 13:12:05 +0100

    Fixed and modified SnakeEnvironment and main
    Fixed observation_space_size
    
    Co-authored-by: XJustUnluckyX <n.delogu@studenti.unisa.it>

--- Main/Environment/SnakeEnvironment.py
+++ Main/Environment/SnakeEnvironment.py
@@ -19,11 +19,12 @@ class SnakeEnv(gym.Env):
         # in prima posizione della tupla
         self.head = self._spawn_head()  # Generiamo casualmente la posizione di inizio
         self.action_space = spaces.Discrete(4)  # Mosse disponibili: 0: Su, 1: Giù, 2: Sx, 3: Dx
-        self.observation_space = spaces.Dict(
+        self.observation_space = spaces.Dict(  # Per ridurre lo spazio delle osservazioni, teniamo traccia delle
+            # features degli stati
             {
                 "head": spaces.Box(low=0, high=self.size - 1, shape=(2,), dtype=int),
-                "actions": spaces.Box(low=-1, high=3, shape=(1 * self.total_size - 1,), dtype=int),
-                "food": spaces.Box(low=0, high=self.size - 1, shape=(2,), dtype=int)
+                "actions": spaces.Box(low=-1, high=3, shape=(grid_size,), dtype=int),
+                "food": spaces.Box(low=0, high=1, shape=(2,), dtype=int)
             }
         )
 
@@ -40,14 +41,20 @@ class SnakeEnv(gym.Env):
     def _update_snake_position(self, action):
         action = self._check_opposite(action)
         if action == 0:  # Su
-            self.head[0] -= 1
+            self.head[1] = self.head[1] + 1
         elif action == 1:  # Giù
-            self.head[0] += 1
+            self.head[1] = self.head[1] - 1
         elif action == 2:  # Sx
-            self.head[1] -= 1
+            self.head[0] = self.head[0] - 1
         elif action == 3:  # Dx
-            self.head[1] += 1
+            self.head[0] = self.head[0] + 1
         self.last_action = action
+
+    def _update_food_direction(self):
+        x1_bool = self.food[0] < self.head[0],  # Food a Sx o a Dx
+        y1_bool = self.food[1] < self.head[1],  # Food Su o Giù
+        directions = [x1_bool, y1_bool]
+        return directions
 
     def _check_opposite(self, action):  # Controlla che l'azione non sia nel verso opposto dell'ultima azione eseguita
         if (self.last_action == 0) and (action == 1):
@@ -72,7 +79,7 @@ class SnakeEnv(gym.Env):
         return head_position
 
     def _update_snake_length(self):
-        self.snake_length = len(self.body)+1
+        self.snake_length = len(self.body) + 1
 
     def _check_collision(self):
         if (
@@ -85,9 +92,9 @@ class SnakeEnv(gym.Env):
             return True
         return False
 
-
     def _get_obs(self):
-        return {"head": self.head, "actions": self.actions, "food": self.food}
+        food_direction = self._update_food_direction()
+        return {"head": self.head, "actions": self.actions, "food": food_direction}
 
     def _get_info(self):
         head_np = np.array(self.head)
@@ -104,12 +111,11 @@ class SnakeEnv(gym.Env):
         self._update_snake_length()
         obs = self._get_obs()
         info = self._get_info()
-        if self.render_mode == "human":
-            self._render_frame()
+        self._render_frame()
         return obs, info
 
     def _increase_actions(self, last_action):
-        i = (self.size * self.size) - 2
+        i = 4
         while i >= 1:
             j = i - 1
             self.actions[i] = self.actions[j]
@@ -117,33 +123,39 @@ class SnakeEnv(gym.Env):
         self.actions[0] = last_action
 
     def step(self, action):
-        previous_head = self.head
+        previous_head = [self.head[0], self.head[1]]
         self._update_snake_position(action)
         self._increase_actions(action)
+        self._update_snake_length()
         done = False
         if self._check_collision():
             reward = -100  # Penalità per collisione
             done = True
-            if self.snake_length > 1:
-                self.body.pop()
-                self.body.append(list(previous_head))  # La testa precedente diventa l'ultimo valore della lista del body
+            self._render_frame()
+            self.body.append([previous_head[0], previous_head[1]])  # La testa precedente diventa l'ultimo valore
+            # della lista del body
+            self.body.pop(0)
             self._update_snake_length()
+
             return self._get_obs(), reward, done, self.snake_length, self._get_info()
-        elif self.head == self.food:
+        if self.head == self.food:
             reward = 10  # Ricompensa per mangiare il cibo
+            self._render_frame()
+            self.body.append([previous_head[0], previous_head[1]])
             self.food = self._generate_food()
         else:
             # Ricompensa intermedia per avvicinamento al cibo
-            reward = -1 * math.dist(self.head, self.food)
-            if self.snake_length > 1:
-                self.body.pop()
-
-        self.body.append(list(previous_head))  # La testa precedente diventa l'ultimo valore della lista del body
-        self._update_snake_length()
-
-        if self.render_mode == "human":
+            center = self.size / 2
+            centered_head = [self.head[0] / center, self.head[1] / center]
+            centered_food = [self.food[0] / center, self.food[1] / center]
+            reward = 0.1 * (2 - math.dist(centered_head, centered_food))
             self._render_frame()
+            self.body.append([previous_head[0], previous_head[1]])  # La testa precedente diventa l'ultimo valore
+            # della lista del body
+            self.body.pop(0)
+        self._render_frame()
 
+        self._update_snake_length()
         return self._get_obs(), reward, done, self.snake_length, self._get_info()
 
     def render(self):
@@ -164,40 +176,51 @@ class SnakeEnv(gym.Env):
                 self.window_size / self.size
         )
 
-        # head
-        pygame.draw.rect(
-            canvas,
-            (0, 255, 0),
-            pygame.Rect(
-                int(pix_square_size * self.head[0]),
-                int(pix_square_size * self.head[1]),
-                int(pix_square_size),
-                int(pix_square_size),
-            ),
-        )
-
-        # body != [-1, -1]
-        for i in range(len(self.body)):
-            if self.body[i] != [-1, -1]:
-                pygame.draw.rect(
-                    canvas,
-                    (0, 0, 255),
-                    pygame.Rect(
-                        int(pix_square_size * self.body[i][0]),
-                        int(pix_square_size * self.body[i][1]),
-                        int(pix_square_size),
-                        int(pix_square_size),
-                    ),
-                )
-
         # food
-        pygame.draw.circle(
-            canvas,
-            (255, 0, 0),
-            (int((self.food[0] + 0.5) * pix_square_size),
-             int((self.food[1] + 0.5) * pix_square_size)),
-            int(pix_square_size / 3),
-        )
+        if self.head != self.food:
+            pygame.draw.circle(
+                canvas,
+                (255, 0, 0),
+                (int((self.food[0] + 0.5) * pix_square_size),
+                 int((self.food[1] + 0.5) * pix_square_size)),
+                int(pix_square_size / 3),
+            )
+            # head
+            pygame.draw.rect(
+                canvas,
+                (0, 255, 0),
+                pygame.Rect(
+                    int(pix_square_size * self.head[0]),
+                    int(pix_square_size * self.head[1]),
+                    int(pix_square_size),
+                    int(pix_square_size),
+                ),
+            )
+        else:
+            # head
+            pygame.draw.rect(
+                canvas,
+                (255, 0, 0),
+                pygame.Rect(
+                    int(pix_square_size * self.head[0]),
+                    int(pix_square_size * self.head[1]),
+                    int(pix_square_size),
+                    int(pix_square_size),
+                ),
+            )
+
+        # body
+        for i in range(len(self.body)):
+            pygame.draw.rect(
+                canvas,
+                (0, 0, 255),
+                pygame.Rect(
+                    int(pix_square_size * self.body[i][0]),
+                    int(pix_square_size * self.body[i][1]),
+                    int(pix_square_size),
+                    int(pix_square_size),
+                ),
+            )
 
         for x in range(self.size + 1):
             pygame.draw.line(
@@ -233,5 +256,5 @@ class SnakeEnv(gym.Env):
             pygame.quit()
 
     def _init_actions(self):
-        actions = np.full((self.size * self.size)-1, 5)
+        actions = np.full(5, 5)
         return actions
--- Main/main.py
+++ Main/main.py
@@ -14,49 +14,47 @@ grid_size = 5
 
 env = gym.make("SnakeQl", grid_size=grid_size, render_mode="human")
 
-observation_space_size = ((grid_size * grid_size) * (grid_size * grid_size)) * (5 * ((grid_size * grid_size) - 1))
+observation_space_size = ((grid_size * grid_size) * 2 * 2) * (5 ** grid_size)
 # Possibili posizioni per la Snake head * possibili
 # posizioni per il cibo * possibili valori delle grid_size^2 -1 azioni
 
-print(observation_space_size)
 action_space = [0, 1, 2, 3]
-
+print(observation_space_size)
 # Grid Search
-learning_rate = [0.1, 0.2, 0.3]
+alpha = [0.5, 0.1, 0.2, 0.3]
 gamma = [0.7, 0.5, 0.4]
-epsilon_start = [0.8, 0.6, 0.4]
-epsilon_decay = [0.99995, 0.995, 0.9]
+epsilon_start = [0.5, 0.8, 0.6, 0.4]
+epsilon_decay = [0.995, 0.9]
 epsilon_min = [0.03, 0.05, 0.1, 0.05]
 best_parameters = []
 
 max_reward_grid_search = -100000
 rel_length = 0
 
-for a in range(len(learning_rate)):
+for a in range(len(alpha)):
     for b in range(len(gamma)):
         for c in range(len(epsilon_start)):
             for d in range(len(epsilon_decay)):
                 for e in range(len(epsilon_min)):
 
                     # Creazione dell'agente Q-learning con i parametri della Grid Search
-                    q_learner = Ql.QLearner(observation_space_size, action_space, learning_rate[a],
-                                                gamma[b], epsilon_start[c], epsilon_decay[d], epsilon_min[e])
+                    q_learner = Ql.QLearner(observation_space_size, action_space, alpha[a],
+                                            gamma[b], epsilon_start[c], epsilon_decay[d], epsilon_min[e])
 
                     # Parametri di addestramento
-                    num_episodes = 500
+                    num_episodes = 10000
                     max_reward = -10000
                     total_score = 0
                     total_reward = 0
                     plot_scores = []
                     plot_mean_scores = []
                     n_games = 0
-                    total_reward = 0
-
+                    temp_max_length = 0
                     # Ciclo di addestramento
                     for episode in range(num_episodes):
                         state, info = env.reset()
                         length = 0
-                        temp_max_length = 0
+                        total_reward = 0
 
                         while True:
                             # Conversione dello stato in un intero, per poter definire la riga corrispondente nella
@@ -78,21 +76,21 @@ for a in range(len(learning_rate)):
                                 if max_reward < total_reward:
                                     max_reward = total_reward
                                     temp_max_length = length
-                                total_score += (length-1)
+                                total_score += (length - 1)
                                 mean_score = total_score / n_games
-                                plot_scores.append(length-1)
+                                plot_scores.append(length - 1)
                                 plot_mean_scores.append(mean_score)
-                                plot(plot_scores, plot_mean_scores)
+                                #plot(plot_scores, plot_mean_scores)
                                 break
 
                         # Aggiornamento del tasso di esplorazione
                         q_learner.decay_epsilon()
 
-                    print(f"Max reward: {max_reward}, last length: {length}")
+                    print(f"Max reward: {max_reward}, relative length: {temp_max_length}")
                     if max_reward_grid_search < max_reward:
                         max_reward_grid_search = max_reward
                         rel_length = temp_max_length
-                        best_parameters = [learning_rate[a], gamma[b], epsilon_start[c], epsilon_decay[d],
+                        best_parameters = [alpha[a], gamma[b], epsilon_start[c], epsilon_decay[d],
                                            epsilon_min[e]]
                         print(f"New best parameters: {best_parameters}")
 
